72637abd4464c139868e8258e0f19b0ffbc1dfd1
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2026-01-04 15:12:16 +0545

    fix: minor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { EasySewa } from './easySewa';
+
 export * from './easySewa';
 export * from './types'
+
+
+// Attach to window for HTML usage
+if (typeof window !== "undefined") {
+    // Expose the class globally
+    (window as any).EasySewa = EasySewa;
+}
